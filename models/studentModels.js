d54be01e5071683f953a951ba075ba91b3c988da
LakshmiSaraswathi2203 <lakshmisaraswathi2203@gmail.com>
2024-08-27 22:05:20 +0530

    adding only necessary files

---
+++ models/studentModels.js
@@ -0,0 +1,4 @@
+// const mongoose = require('mongoose');
+// const studentSchema = require('../db/schemas/studentSchema');
+// const Student = mongoose.model('Student', studentSchema);
+// module.exports = Student;   
